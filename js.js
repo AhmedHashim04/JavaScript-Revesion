8fa8ba36c80fdf9cde54be75b7ce41347bec47c3
Ahmed Hashim <ahmedha4im7@gmail.com>
2025-04-12 02:14:05 +0200

    15 video el-zero course

---
+++ js.js
@@ -0,0 +1,98 @@
+// var x = 50 ;
+
+// It is Comment on One line
+
+/*
+it is multiline comment
+*/
+
+// console.log("hey")
+// document.write("werwe")
+
+// Lesson 8 / 188
+
+// console.error("sadasd");
+// // console.table(['A','B','C'])
+// console.log('%cHHHHH  %cADsad', 'color:red;font-size:40px','color:blue;font-size:40px');
+
+// Lesson 9-10 / 188
+
+let x = "Mome";
+
+console.log(`Hello ${x}`);
+console.log(typeof'HelloHello');
+console.log(typeof (50));
+console.log(typeof 50);
+console.log(typeof 7000.50);
+
+console.log(typeof ["fdgf" , "gdfhdf" ,"tt"]) ;//object -->array
+console.log(typeof ["fdgf" , "gdfhdf" ,40]) ;//object -->array
+
+
+console.log(typeof {'name':"Ali" , 'age':40}); //object --> object
+console.log(typeof true); //boolean
+console.log(typeof false); 
+console.log(typeof True); //undefined
+console.log(typeof undefined); //undefined
+console.log(typeof null); //object
+
+
+// Lesson 11 / 188
+
+var name ="Osama" ,age ='177';
+
+
+console.log(jgo); 
+
+// jgo.innerHTML ="qqqqqqq";
+
+
+// Lesson 12 / 188
+
+var _varby_in_begin = "_varby_in_uuuuus1begin"
+var $varby_in$begin = "_varby_in111_begin"
+
+console.log(_varby_in_begin); 
+
+console.log($varby_in$begin); 
+
+
+// Lesson 13 / 188
+
+ // الفروق بين const و var و let في JavaScript:
+
+// 1. const (ثابت):
+// - لا يمكن تغيير قيمته بعد التعريف
+// - يجب إعطاؤه قيمة عند التعريف
+// - له نطاق كتلة (block scope)
+
+// 2. let (متغير):
+// - (block scope)
+
+// - لا يمكن إعادة تعريفه في نفس النطاق
+// let a ="we love";
+// let a ="we 4"; Error
+
+// 3. var (متغير قديم):
+// - (function scope)
+// - يمكن إعادة تعريفه
+// var a ="we 4";
+// var a ="we love";excute normaly
+// - يتم رفعه (hoisted) إلى أعلى النطاق
+
+// Lesson 14 / 188
+
+var a ="we 4";
+var a ="we love";
+let b ="python";
+
+console.log("asdasd"+"nbvnvbnvb"+"yyyy");
+console.log(a+" "+b);
+console.log(a,b);
+
+// Lesson 15 / 188
+console.log(a+
+    " 55"+b);
+
+console.log(`Hello 
+    ${x}`);
